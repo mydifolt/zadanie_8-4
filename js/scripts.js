a3b231df8f3d582e46cf2dee65f6dbae8102ab22
Piotrek <mydifolt@gmail.com>
2017-05-14 15:12:03 +0200

    try no 3

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// scripts.js file
+// There is a linear comment here, we can not use enter,
+// unless we do the next one again
+
+// alert('test');
+// the function above has been commented and will not be executed
+
+var imie = prompt('Wpisz prosze swoje imie');
+// alert('Witaj, ' + imie);
+// dowolnym typem komentarza
+console.log('Witaj, ' + imie);
+//Dodaję jeszcze dwa inne komentarze.
+/* Stworzyłem nowe repozytorium i wysłałem tam swój projekt. 
+Pamiętam o udostępnieniu go Mentorowi na Githubie.
+Link do projektu wstawiłem na końcu zadania.
+*/
+ 
